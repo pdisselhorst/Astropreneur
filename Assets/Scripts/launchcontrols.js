11c928e15ea99e09601f918add63b43aca86dc08
Paul <paul.disselhorst@gmail.com>
2013-03-16 00:42:01 -0700

    Thrust controls working
    Buttons will move rocket on click

--- Assets/Scripts/launchcontrols.js
+++ Assets/Scripts/launchcontrols.js
@@ -3,8 +3,15 @@ var showLaunch = true;
 function OnGUI ()
 {
 
-GUI.Box (Rect (300,550,100,40), "Thrust Left");
-GUI.Box (Rect (525,550,100,40), "Thrust Right");
+if(GUI.Button (Rect (300,550,100,40), "Thrust Left"))
+{
+	transform.Translate (Vector3(1,0,0) * Time.deltaTime*launch.speed);
+	}
+if(GUI.Button (Rect (525,550,100,40), "Thrust Right"))
+{
+    transform.Translate (Vector3(-1,0,0) * Time.deltaTime*launch.speed);
+    }
+
 
 if(showLaunch)
 if(GUI.Button (Rect (10,40,80,30), "Launch"))
